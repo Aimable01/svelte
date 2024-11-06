88cab81901937f05b37405537bfebdfde64765e5
Aimable01 <aimable.kwizera14@gmail.com>
2024-11-06 17:21:54 +0200

    feat: navlinks

---
+++ svelte-kit/src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    export let navLinks = [
+        {link:"/", name: "Home"},
+        {link:"/posts", name: "Posts"},
+    ]
+</script>
+
+<nav>
+   {#each navLinks as navlink}
+    <a href={navlink.link}>{navlink.name}</a>
+   {/each}
+</nav>
+
+<slot />
